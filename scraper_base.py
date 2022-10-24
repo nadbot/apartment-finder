ae38fefd07258ca052165bbdc4958b393ffb49b3
Daniel <daniel@numerico.tech>
2022-10-24 10:55:02 +0200

    Extracted common functions to scraperBase class

---
+++ scraper_base.py
@@ -0,0 +1,85 @@
+import json
+import os
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+
+class ScraperBase:
+    def __init__(self, header, baseurl, startpage, links_file, csv_file):
+        self.header = header
+        self.BASE_URL = baseurl
+        self.url = self.BASE_URL + startpage
+        self.links_file = links_file
+        self.csv_file = csv_file
+
+    def get_data(self, url, data_format=None):
+        res = requests.get(url, headers=self.header)
+        if res.status_code > 299:
+            raise ConnectionError("Could not get data")
+        if not data_format:
+            return res.text
+        if data_format == 'json':
+            return json.loads(res.text)
+        return BeautifulSoup(res.text, 'html.parser')
+
+    def get_all_overview_pages(self, url, data):
+        return []
+
+    def get_all_apartments_per_page(self, data):
+        return []
+
+    def get_all_apartments(self):
+        data = self.get_data(self.url, 'html')
+        all_pages = self.get_all_overview_pages(self.url, data)
+        all_apartments = []
+        for page in all_pages:
+            data = self.get_data(page, 'html')
+            all_apartments.extend(self.get_all_apartments_per_page(data))
+        with open(self.links_file, 'w') as f:
+            f.write(','.join(all_apartments))
+        return all_apartments
+
+    def get_apartment_details(self, apartment_url, data):  # TODO
+        return {}
+
+    def get_all_apartment_data(self, urls):
+        all_data = []
+        for apartment_url in tqdm(urls):
+            data = self.get_data(apartment_url, 'html')
+            try:
+                data_dict = self.get_apartment_details(apartment_url, data)
+            except Exception as e:
+                print(f'Failed to retrieve data for {apartment_url}')
+                continue
+            all_data.append(data_dict)
+        if len(all_data) == 0:
+            return
+        df = pd.DataFrame(all_data)
+        if not os.path.isfile(self.csv_file):
+            df.to_csv(self.csv_file, index=False)
+        else:
+            df.to_csv(self.csv_file, mode='a', header=False, index=False)
+        return df
+
+    def update_apartments(self, rerun_all=False):
+        """If check_available, rescrape everything, otherwise just add the new ones."""
+        try:
+            with open(self.links_file, 'r') as f:
+                all_apartments_old = set(f.read().split(','))
+        except (FileNotFoundError, TypeError) as e:
+            print(e)
+            rerun_all = True
+        all_apartments_now = set(self.get_all_apartments())
+        if rerun_all:
+            return all_apartments_now
+        missing_apartments = all_apartments_now - all_apartments_old
+        return missing_apartments
+
+    def scrape(self, rerun_all=False):
+        apartments_to_scrape = self.update_apartments(rerun_all)
+        print(f'Updating {len(apartments_to_scrape)} entries')
+        df = self.get_all_apartment_data(apartments_to_scrape)
+        return df
--- scraper_pararius.py
+++ scraper_pararius.py
@@ -1,10 +1,4 @@
-import json
-import os.path
-
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-from tqdm import tqdm
+from scraper_base import ScraperBase
 
 headers_pararius = {
     # 'path': /apartments/utrecht/0-1750/page-3
@@ -25,104 +19,45 @@ headers_pararius = {
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
 }
-PARARIUS_BASE_URL = 'https://www.pararius.com'
 
 
-def get_data(url, data_format=None):
-    res = requests.get(url, headers=headers_pararius)
-    if res.status_code > 299:
-        raise ConnectionError("Could not get data")
-    if not data_format:
-        return res.text
-    if data_format == 'json':
-        return json.loads(res.text)
-    return BeautifulSoup(res.text, 'html.parser')
+class ScraperPararius(ScraperBase):
+    def __init__(self):
+        header = headers_pararius
+        baseurl = 'https://www.pararius.com'
+        startpage = '/apartments/utrecht/0-1750/'
+        links_file = 'data/all_links_pararius.txt'
+        csv_file = 'data/apartment_data_pararius.csv'
+        super().__init__(header, baseurl, startpage, links_file, csv_file)
 
+    def get_all_overview_pages(self, url, data):
+        max_pages = int(
+            # -2 because last one is the link for next page, rsplit to split at
+            data.find_all("ul", {"class": "pagination__list"})[0].find_all('a')[-2].get('href').split('page-')[1])
+        new_urls = [url + f'page-{page}' for page in range(1, max_pages + 1)]
+        return new_urls
 
-def get_all_overview_pages_pararius(url, data):
-    max_pages = int(
-        # -2 because last one is the link for next page, rsplit to split at
-        data.find_all("ul", {"class": "pagination__list"})[0].find_all('a')[-2].get('href').split('page-')[1])
-    new_urls = [url + f'page-{page}' for page in range(1, max_pages + 1)]
-    return new_urls
+    def get_all_apartments_per_page(self, data):
+        apartments = data.find_all('h2', {'class': 'listing-search-item__title'})
+        links = set(self.BASE_URL + apartment.a.get('href') for apartment in apartments)
+        return links
 
-
-def get_all_apartments_per_page(data):
-    apartments = data.find_all('h2', {'class': 'listing-search-item__title'})
-    links = set(PARARIUS_BASE_URL + apartment.a.get('href') for apartment in apartments)
-    return links
-
-
-def get_all_apartments_pararius():
-    url_pararius = PARARIUS_BASE_URL + '/apartments/utrecht/0-1750/'
-    data = get_data(url_pararius, 'html')
-    all_pages = get_all_overview_pages_pararius(url_pararius, data)
-    all_apartments = []
-    for page in all_pages:
-        data = get_data(page, 'html')
-        all_apartments.extend(get_all_apartments_per_page(data))
-    with open('data/all_links_pararius.txt', 'w') as f:
-        f.write(','.join(all_apartments))
-    return all_apartments
-
-
-def get_apartment_details(url, data):  # TODO
-    data_dict = {
-        'address': data.find('h1', {'class': 'listing-detail-summary__title'}).text.split('For rent:')[1].strip(),
-        'postcode': data.find('div', {'class': 'listing-detail-summary__location'}).text.strip()
+    def get_apartment_details(self, apartment_url, data):
+        data_dict = {
+            'address': data.find('h1', {'class': 'listing-detail-summary__title'}).text.split('For rent:')[1].strip(),
+            'postcode': data.find('div', {'class': 'listing-detail-summary__location'}).text.strip()
         }
-    # squaremeter = data.find('span', {'class': 'kenmerken-highlighted__value fd-text--nowrap'}).text.strip()
-    datasheets = data.find_all('dl', {'class': 'listing-features__list'})
-    for datasheet in datasheets:
-        a = datasheet.find_all(['dt', 'dd'])
-        s = [i.text.strip() for i in a if len(i.find_all(['dt', 'dd'])) == 0]
-        for index in range(0, len(s) - 1, 2):
-            data_dict[s[index]] = s[index + 1]
-    data_dict['url'] = url
-    return data_dict
-
-
-def get_all_apartment_data_pararius(urls):
-    all_data = []
-    for apartment_url in tqdm(urls):
-        data = get_data(apartment_url, 'html')
-        try:
-            data_dict = get_apartment_details(apartment_url, data)
-        except Exception as e:
-            print(f'Failed to retrieve data for {apartment_url}')
-            continue
-        all_data.append(data_dict)
-    if len(all_data) == 0:
-        return
-    df = pd.DataFrame(all_data)
-    if not os.path.isfile(csv_file := 'data/apartment_data_pararius.csv'):
-        df.to_csv(csv_file, index=False)
-    else:
-        df.to_csv(csv_file, mode='a', header=False, index=False)
-    return df
-
-
-def update_apartments(rerun_all=False):
-    """If check_available, rescrape everything, otherwise just add the new ones."""
-    try:
-        with open('data/all_links_pararius.txt', 'r') as f:
-            all_apartments_old = set(f.read().split(','))
-    except (FileNotFoundError, TypeError) as e:
-        print(e)
-        rerun_all = True
-    all_apartments_now = set(get_all_apartments_pararius())
-    if rerun_all:
-        return all_apartments_now
-    missing_apartments = all_apartments_now - all_apartments_old
-    return missing_apartments
-
-
-def scrape_pararius(rerun_all=False):
-    apartments_to_scrape = update_apartments(rerun_all)
-    print(f'Updating {len(apartments_to_scrape)} entries')
-    df = get_all_apartment_data_pararius(apartments_to_scrape)
-    return df
+        # squaremeter = data.find('span', {'class': 'kenmerken-highlighted__value fd-text--nowrap'}).text.strip()
+        datasheets = data.find_all('dl', {'class': 'listing-features__list'})
+        for datasheet in datasheets:
+            a = datasheet.find_all(['dt', 'dd'])
+            s = [i.text.strip() for i in a if len(i.find_all(['dt', 'dd'])) == 0]
+            for index in range(0, len(s) - 1, 2):
+                data_dict[s[index]] = s[index + 1]
+        data_dict['url'] = apartment_url
+        return data_dict
 
 
 if __name__ == '__main__':
-    scrape_pararius(False)
+    scraper = ScraperPararius()
+    scraper.scrape(rerun_all=True)
